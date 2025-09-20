6b02cc91d5e20d48d816d6b1ce49b688275d3289
mariana-ardila <desarrolladoraardila@gmail.com>
2025-09-20 14:49:30 -0500

    (develop): Validación del formulario con expresiones regulares

---
+++ helpers/expresiones.js
@@ -0,0 +1,5 @@
+export let expresiones={
+    texto:/^[A-Za-zñÑáéíóúÁÉÍÓÚüÜ\s]{5,50}$/,
+    numero:/^\d{5,10}$/
+}
+
--- helpers/validarRegistro.js
+++ helpers/validarRegistro.js
@@ -1,7 +1,4 @@
-/* 
-querySelectorAll
-
-*/
+import {expresiones} from "./expresiones.js";
 
 let inputs = document.getElementsByClassName("formulario__entrada")
 let arregloInputs = [...inputs]
@@ -9,5 +6,35 @@ console.log(inputs)
 console.log(arregloInputs)
 
 arregloInputs.map((input)=> {
-    console.log(input)
+    input.addEventListener("keyup", (e)=>{
+        switch (e.target.id){
+            case "nombre":
+                if(expresiones.texto.test(e.target.value)){
+                    e.target.style.borderColor="green";
+                    e.target.style.backgroundColor="salmon"
+                    // document.getElementById("nombre").style.borderColor="green";
+                }else{
+                    document.getElementById("nombre").style.borderColor="red";
+                }
+            break;
+            case "apellido":
+                if(expresiones.texto.test(e.target.value)){
+                    e.target.classList.add("correcto")
+                    e.target.classList.remove("incorrecto")
+                }else{
+                    e.target.classList.add("incorrecto")
+                    e.target.classList.remove("correcto")
+                }
+                    
+            break;
+
+            case "documento":
+                if(expresiones.numero.test(e.target.value)){
+                     e.target.classList.add("correcto")
+                    e.target.classList.remove("incorrecto")
+                }else{
+                    e.target.classList.add("incorrecto")
+                    e.target.classList.remove("correcto")
+                }
+}     })
 })
